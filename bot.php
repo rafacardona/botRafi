de2ff3d843da9788fb900eed3b473ccadb35cd74
rafacardona <33635596+rafacardona@users.noreply.github.com>
2023-03-02 12:35:27 +0100

    Mensajes basicos bot
    Primeros mensajes basicos para el bot

--- bot.php
+++ bot.php
@@ -1,5 +1,39 @@
 <?php
 
 $token = '6094274678:AAGXpcPqDIfQ9sju8canKuUthddyeKblC1c';
+$website = 'https://api.telegram.org/bot' . $token;
 
-?>
+//lo que el bot recibe
+$input = file_get_contents('php://input');
+
+//descodifico json recibido
+$update = json_decode($input, true);
+
+$chatId = $update['message']['chat']['id'];
+//mensaje recibido
+$message = $update['message']['text'];
+
+//en funcion del mensage recibido hago una respuesta.
+switch ($message) {
+    case '/start':
+        $response = 'Hola! Estas en el bot de Rafi!';
+        sendMessage($chatId, $response);
+        break;
+    case '/info':
+        $response = 'SIII!, correcto estas en el bot de Rafi!';
+        sendMessage($chatId, $response);
+        break;
+    default:
+        $response = 'No entiendo :( ';
+        sendMessage($chatId, $response);
+        break;
+}
+
+
+//funcion para enviar un mensaje de respuesta que recibe el id del chat y el mensaje.
+function sendMessage($chatId, $response){
+    
+    $url = $GLOBALS['website'].'/sendMessage?chat_id='.$chatId.'&parse_mode=HTML&text='.urlencode($response);
+    
+    file_get_contents($url);
+}
